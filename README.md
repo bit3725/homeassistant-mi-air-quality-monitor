fffbf4049227db253898804a1d5e4aab8c3a17c2
xingkui wang <bit3725@gmail.com>
2017-11-23 13:57:20 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # homeassistant-mi-air-quality-monitor
-XiaoMi Air Quality Monitor component for Home Assistant.
+XiaoMi Air Quality Monitor(PM2.5 sensor) component for Home Assistant.
+
+![Screenshot](https://raw.githubusercontent.com/bit3725/homeassistant-mi-air-quality-monitor/master/images/screenshot.jpg)
+
+## Installation
+1. Copy *custom_components/sensor/mi_air_quality_moitor.py* to **.homeassistant/custom_components/sensor**.
+2. Get the IP of your sensor.
+3. Follow [Retrieving the Access Token](https://home-assistant.io/components/vacuum.xiaomi_miio/#retrieving-the-access-token) guide to get the token of your sensor
+
+## Configuration
+```yaml
+sensor:
+  - platform: mi_air_quality_monitor
+    host: YOUR_SENSOR_IP
+    token: YOUR_SENSOR_TOKEN
+    name: YOUT_SENSOR_NAME
+```
---
+++ custom_components/sensor/mi_air_quality_monitor.py
@@ -0,0 +1,80 @@
+"""Support for Xiaomi air quality monitor."""
+import logging
+
+from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_TOKEN, )
+from homeassistant.helpers.entity import Entity
+from homeassistant.exceptions import PlatformNotReady
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = ['python-miio==0.3.2']
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Perform the setup for Xiaomi air quality monitor."""
+    from miio import AirQualityMonitor, DeviceException
+
+    host = config.get(CONF_HOST)
+    name = config.get(CONF_NAME)
+    token = config.get(CONF_TOKEN)
+
+    _LOGGER.info("Initializing Xiaomi air quality monitor with host %s (token %s...)", host, token[:5])
+
+    devices = []
+    try:
+        airQualityMonitor = AirQualityMonitor(host, token)
+        airQualityMonitorSensor = XiaomiAirQualityMonitorSensor(airQualityMonitor, name)
+        devices.append(airQualityMonitorSensor)
+    except DeviceException:
+        raise PlatformNotReady
+
+    add_devices(devices)
+
+class XiaomiAirQualityMonitorSensor(Entity):
+    """Representation of a XiaomiAirQualityMonitorSensor."""
+
+    def __init__(self, airQualityMonitor, name):
+        """Initialize the XiaomiAirQualityMonitorSensor."""
+        self._state = None
+        self._name = name
+        self._airQualityMonitor = airQualityMonitor
+        self.parse_data()
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return 'mdi:cloud'
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return 'AQI'
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the last update."""
+        attrs = {}
+        attrs['Battery'] = '{}%'.format(self._airQualityMonitor.status().battery)
+        attrs['USB Power'] = 'On' if self._airQualityMonitor.status().usb_power else 'Off'
+
+        return attrs
+
+    def parse_data(self):
+        try:
+            self._state = self._airQualityMonitor.status().aqi
+        except DeviceException:
+            _LOGGER.exception('Fail to get aqi from Xiaomi Air Quality Monitor')
+            raise PlatformNotReady
+
+    def update(self):
+        """Get the latest data and updates the states."""
+        self.parse_data()
